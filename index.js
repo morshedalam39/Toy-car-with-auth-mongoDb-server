bd31a3b6955223e27ae0c28744d1339809ead84f
morshedalam39 <morshedalam4239@gmail.com>
2023-05-19 19:43:03 +0600

    data deleted succes

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ async function run() {
         res.send(result);
     });
 
+    // deleteOne
+
     app.delete('/myToy/:id', async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) }
