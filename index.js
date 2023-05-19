ef8ad3eae520a375a5313d451a246c87f996c7e9
morshedalam39 <morshedalam4239@gmail.com>
2023-05-19 16:03:29 +0600

    Post successfuly added

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('toyCarDB').collection('allToys');
+
+
+
+    app.post('/toys', async (req, res) => {
+        const toys = req.body;
+        console.log(toys);
+        const result = await toysCollection.insertOne(toys);
+        res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
